f842f829aa40118b55efb8dd05fdb3e582936c6a
Jeth47 <markjapeth.lariba@g.msuiit.edu.ph>
2022-04-27 23:39:49 +0800

    1st commit

---
+++ src/Teacher.java
@@ -0,0 +1,12 @@
+
+public class Teacher {
+ String firstName;
+ String lastName;
+ String idNumber;
+ 
+ public Teacher(String firstName, String lastName, String idNumber) {
+	 this.firstName = firstName;
+	 this.lastName = lastName;
+	 this.idNumber = idNumber;
+ }
+}
