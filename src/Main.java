599bda6e53c9cca83c941e983b0f43d57113ca5d
rcseven <rcedquila@gmail.com>
2022-04-27 21:05:12 +0800

    first Commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		Student s1 = new Student();
+		Teacher t1 = new Teacher();
+		
+		System.out.println("Student Name: " + s1.idNumber + " " + s1.lastName + ", " + s1.firstName);
+		System.out.println("Teacher Name: " + t1.idNumber + " " + t1.lastName + ", " + t1.firstName);
+	}
+
+}
