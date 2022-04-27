1b289eecca2bdf0d781e7a37201b6ca83edfd7c1
liamgluna <74529953+liamgluna@users.noreply.github.com>
2022-04-27 22:02:31 +0800

    1st commit

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@
 public class Main {
 
 	public static void main(String[] args) {
-		Student s1 = new Student();
+		Student s1 = new Student("Liam", "Luna", "2019-1100");
 		Teacher t1 = new Teacher();
 		
 		System.out.println("Student Name: " + s1.idNumber + " " + s1.lastName + ", " + s1.firstName);
